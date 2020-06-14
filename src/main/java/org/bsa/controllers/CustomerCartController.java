cf34f98ffd751a164699770287c520fb5928f001
Bianka Beuka <beuka_bianka@yahoo.com>
2020-06-14 17:21:47 +0300

    BSA-54 Show the selected services in 'My cart' Page

--- src/main/java/org/bsa/controllers/CustomerCartController.java
+++ src/main/java/org/bsa/controllers/CustomerCartController.java
@@ -7,17 +7,40 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import org.bsa.model.Service;
 
+import javax.swing.text.TabableView;
 import java.io.IOException;
 
 
-public class CustomerCartController {
+public class CustomerCartController  {
+    @FXML
+    TableView<Service> tableCart;
+    @FXML
+    TableColumn serviceColumn;
+    @FXML
+    TableColumn priceColumn;
     @FXML
     Button backButton;
     static ObservableList<Service> selectedservices= FXCollections.observableArrayList();
+
     public void initialize(){
+        CustomerServicesListController s=new CustomerServicesListController();
+        ObservableList<Service> services = FXCollections.observableArrayList();
+        services=s.getSelected();
+        tableCart.setItems(services);
+      // System.out.print(services+"\n ");
+        initCols();
+
+    }
+    public void initCols(){
+        serviceColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+        priceColumn.setCellValueFactory(new  PropertyValueFactory<>("price"));
+
     }
     public void setSelectedservices(ObservableList<Service> s){selectedservices=s;}
     public void handleBackButton(){

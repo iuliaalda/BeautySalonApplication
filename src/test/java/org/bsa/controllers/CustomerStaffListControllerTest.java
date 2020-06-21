0d7d35b6c8e90f5adb2ae4aa9e17057f6ff94f43
Iulia Alda <aldaiulia99@gmail.com>
2020-06-21 10:36:59 +0300

    Added initialize test code

--- src/test/java/org/bsa/controllers/CustomerStaffListControllerTest.java
+++ src/test/java/org/bsa/controllers/CustomerStaffListControllerTest.java
@@ -1,5 +1,6 @@
 package org.bsa.controllers;
 
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import org.apache.commons.io.FileUtils;
@@ -32,11 +33,19 @@ public class CustomerStaffListControllerTest {
         FileUtils.cleanDirectory(FileSystemService.getApplicationHomePath().toFile());
         FileUtils.copyURLToFile(Employee.class.getClassLoader().getResource("employees.json"),new File("src/test/resources/employees.json"));
         controller=new CustomerStaffListController();
+        controller.tableStaff=new TableView<>();
+        controller.fName=new TableColumn<>();
+        controller.lName=new TableColumn<>();
+        controller.age=new TableColumn<>();
+        controller.service=new TableColumn<>();
+        controller.yrsExp=new TableColumn<>();
     }
     @Test
     public void initialize() throws Exception{
         EmployeeService.loadEmployees(efile);
         assertNotNull(EmployeeService.returnEmp());
+        controller.initialize();
+        assertEquals("[Employee{firstName='Iulia', lastName='Alda', age=20, serviceType='makeup', yearsExperience=2, listServices=[Service{type='Simple Makeup', price=50.0, empl='Iulia'}, Service{type='Bridal Makeup', price=60.0, empl='Iulia'}]}, Employee{firstName='Bianka', lastName='Beuka', age=21, serviceType='hair stylist', yearsExperience=3, listServices=[Service{type='Long Hairstyle', price=55.0, empl='Bia'}]}]",controller.tableStaff.getItems().toString());
     }
 
     @Test

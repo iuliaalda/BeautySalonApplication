4120304c0dcd1c2cb9167d11a6cf3632ce043210
Bianka Beuka <beuka_bianka@yahoo.com>
2020-06-21 03:53:48 +0300

    Testing 'AppointmentsEmployeeController' class

--- src/test/java/org/bsa/controllers/AppointmentsEmployeeControllerTest.java
+++ src/test/java/org/bsa/controllers/AppointmentsEmployeeControllerTest.java
@@ -5,9 +5,7 @@ import org.apache.commons.io.FileUtils;
 import org.bsa.Main;
 import org.bsa.model.Appointment;
 import org.bsa.model.Employee;
-import org.bsa.model.Service;
 import org.bsa.service.AppointmentService;
-import org.bsa.service.EmployeeService;
 import org.bsa.service.FileSystemService;
 import org.bsa.service.ServicesService;
 import org.junit.Before;
@@ -20,49 +18,56 @@ import java.io.IOException;
 
 import static org.junit.Assert.*;
 
-public class CustomerViewAppointmentsPageTest {
+public class AppointmentsEmployeeControllerTest {
+
     File afile = new File("src\\test\\resources\\appointments.json");
     File sfile = new File("src\\test\\resources\\services.json");
-    private CustomerViewAppointmentsPage controller;
-    public static final String TEXT = "My Appointments";
+    private AppointmentsEmployeeController controller;
     @BeforeClass
     public static void setupClass() throws Exception {
         FileSystemService.APPLICATION_FOLDER=".test-example";
         FileSystemService.initApplicationHomeDirIfNeeded();
-        AppointmentService.setUsr("Iulia");
-        AppointmentService.setClientusr("B");
+        ServicesService.setUsr("Bia");
+        AppointmentService.setUsr("Bia");
         //ApplicationTest.launch(Main.class);
     }
     @Before
     public void setUp() throws Exception {
         FileUtils.cleanDirectory(FileSystemService.getApplicationHomePath().toFile());
-        ServicesService.loadServices(afile);
-        FileUtils.copyURLToFile(Appointment.class.getClassLoader().getResource("appointments.json"),new File("src/test/resources/appointments.json"));
+        ServicesService.loadServices(sfile);
         FileUtils.copyURLToFile(Employee.class.getClassLoader().getResource("employees.json"),new File("src/test/resources/employees.json"));
         FileUtils.copyURLToFile(Employee.class.getClassLoader().getResource("services.json"),new File("src/test/resources/services.json"));
-        controller=new CustomerViewAppointmentsPage();
-
+        FileUtils.copyURLToFile(Appointment.class.getClassLoader().getResource("appointments.json"),new File("src/test/resources/appointments.json"));
+        /*controller=new AppointmentsEmployeeController();
+       controller.sfile=sfile;
+       controller.afile=afile;
+       controller.TodayAppsTableView.setItems(AppointmentService.returnTodayApps());*/
     }
     @Test
     public void initialize() throws IOException {
         AppointmentService.loadAppointments(afile);
-        ServicesService.loadServices(sfile);
-        //controller.dateColumn.setText("2020-06-26 11:00");
-        assertEquals(3,AppointmentService.returnMyAppointments().size());
-        assertNotNull(AppointmentService.returnMyAppointments());
+        AppointmentService.returnTodayApps();
+
     }
-
-
 
     @Test
     public void initTable() throws IOException {
         AppointmentService.loadAppointments(afile);
-        AppointmentService.returnMyAppointments();
-        assertNotNull(AppointmentService.returnMyAppointments());
+        AppointmentService.returnTodayApps();
+        assertEquals(0,AppointmentService.returnTodayApps().size());
+        //controller.TodayAppsTableView.setItems(AppointmentService.returnTodayApps());
+       // assertEquals(0,AppointmentService.returnTodayApps().size());
     }
 
     @Test
-    public void handleGoBackToCart() throws IOException {
-        ServicesService.loadServices(sfile);
+    public void initCols() {
+    }
+
+    @Test
+    public void handleAppsCancelledAppsButton() {
+    }
+
+    @Test
+    public void gandleBacktoEmployeePage() {
     }
 }
